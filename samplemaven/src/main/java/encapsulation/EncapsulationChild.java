fc42cdf29146797d8eb229ce553815b7da31d7ba
ashu5 <ashu5@DESKTOP-FJJ601V.bbrouter>
2024-12-22 23:30:21 +0530

    Encapsulation Program

---
+++ samplemaven/src/main/java/encapsulation/EncapsulationChild.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationChild {
+
+	public static void main(String[] args) {
+		
+		EncapsulationParent obj=new EncapsulationParent();
+		obj.setter("Aswathy", 30);
+		obj.getter();
+	}
+
+}
